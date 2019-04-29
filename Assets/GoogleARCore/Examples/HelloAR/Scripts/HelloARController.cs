17e047782d424550b46260b43417314698291c53
techtopus <44426831+techtopus@users.noreply.github.com>
2019-04-29 21:39:31 +0530

    last

--- Assets/GoogleARCore/Examples/HelloAR/Scripts/HelloARController.cs
+++ Assets/GoogleARCore/Examples/HelloAR/Scripts/HelloARController.cs
@@ -11,63 +11,25 @@
     // Set up touch input propagation while using Instant Preview in the editor.
     using Input = InstantPreviewInput;
 #endif
-   
-    /// <summary>
-    /// Controls the HelloAR example.
-    /// </summary>
+
     public class HelloARController : MonoBehaviour
     {
      
         public Button BR, BL,BS,Set,Back,MLeft,MRight;
         public Text hint,rotate,move;
-        int count = 0,setcount=0,item;
-        /// <summary>
-        /// The first-person camera being used to render the passthrough camera image (i.e. AR background).
-        /// </summary>
+        int count = 0,c=0,setcount=0,item;
         public Camera FirstPersonCamera;
-
-        /// <summary>
-        /// A prefab for tracking and visualizing detected planes.
-        /// </summary>
         public GameObject DetectedPlanePrefab;
-
-        /// <summary>
-        /// A model to place when a raycast from a user touch hits a plane.
-        /// </summary>
-        public GameObject AndyPlanePrefab, offchair, chandler, table, bed, bench, archair, archair2, stand, bcase, sink, sofa, couch, stove, fridge, couch2;
-
-        /// <summary>
-        /// A model to place when a raycast from a user touch hits a feature point.
-        /// </summary>
-        //public GameObject AndyPointPrefab;
+        public GameObject offchair, chandler, table, bed, bench, archair, archair2, stand, bcase, sink, sofa, couch, stove, fridge, couch2;
         public GameObject prefab;
-        /// <summary>
-        /// A gameobject parenting UI for displaying the "searching for planes" snackbar.
-        /// </summary>
         public GameObject SearchingForPlaneUI;
-
-        /// <summary>
-        /// The rotation in degrees need to apply to model when the Andy model is placed.
-        /// </summary>
         private const float k_ModelRotation = 180.0f;
         public TrackableHit hit;
         public TrackableHitFlags raycastFilter = TrackableHitFlags.PlaneWithinPolygon |
             TrackableHitFlags.FeaturePointWithSurfaceNormal;
-        /// <summary>
-        /// A list to hold all planes ARCore is tracking in the current frame. This object is used across
-        /// the application to avoid per-frame allocations.
-        /// </summary>
         private List<DetectedPlane> m_AllPlanes = new List<DetectedPlane>();
-
-        /// <summary>
-        /// True if the app is in the process of quitting due to an ARCore connection error, otherwise false.
-        /// </summary>
         private bool m_IsQuitting = false;
-        public Vector3 moveVector;
-        /// <summary>
-        /// The Unity Update() method.
-        /// </summary>
-        GameObject andyObject;
+        public GameObject andyObject;
         public void Scrnbck()
         {
             SceneManager.LoadScene("SelectionMenu");
@@ -98,18 +60,12 @@
         }
         public void PlaceObject()
         {
-            // Instantiate Andy model at the hit pose.
             andyObject = Instantiate(prefab, hit.Pose.position, hit.Pose.rotation);
             DetectedPlanePrefab.SetActive(false);
-            // Compensate for the hitPose rotation facing away from the raycast (i.e. camera).
             andyObject.transform.Rotate(0, k_ModelRotation, 0, Space.Self);
-
-            // Create an anchor to allow ARCore to track the hitpoint as understanding of the physical
-            // world evolves.
             var anchor = hit.Trackable.CreateAnchor(hit.Pose);
             BS.gameObject.SetActive(false);
             Set.gameObject.SetActive(true);           
-            // Make Andy model a child of the anchor.
             andyObject.transform.parent = anchor.transform;
 
         }
@@ -134,8 +90,6 @@
         public void Update()
         {
             _UpdateApplicationLifecycle();
-
-            // Hide snackbar when currently tracking at least one plane.
             Session.GetTrackables<DetectedPlane>(m_AllPlanes);
             bool showSearchingUI = true;
             for (int i = 0; i < m_AllPlanes.Count; i++)
@@ -143,41 +97,44 @@
                 if (m_AllPlanes[i].TrackingState == TrackingState.Tracking)
                 {
                     showSearchingUI = false;
+
+                    if (c == 0)
+                    {
+                        hint.gameObject.SetActive(true);
+                        c++;
+                    }
                     break;
                 }
             }
-
             SearchingForPlaneUI.SetActive(showSearchingUI);
-            hint.gameObject.SetActive(true);
-            // If the player has not touched the screen, we are done with this update.
             Touch touch;
             if (Input.touchCount < 1 || (touch = Input.GetTouch(0)).phase != TouchPhase.Began)
             {
+                
                 return;
             }
+
             if (count == 0)
-            {
-                
+            { 
+               
                 if (Frame.Raycast(touch.position.x, touch.position.y, raycastFilter, out hit))
                 {
-                    hint.gameObject.SetActive(false);
+                   
                     count++;
-                    // Use hit pose and camera pose to check if hittest is from the
-                    // back of the plane, if it is, no need to create the anchor.
                     if ((hit.Trackable is DetectedPlane) &&
                         Vector3.Dot(FirstPersonCamera.transform.position - hit.Pose.position,
                             hit.Pose.rotation * Vector3.up) < 0)
                     {
                         Debug.Log("Hit at back of the current DetectedPlane");
+
                     }
                     else
                     {
-                        hint.gameObject.SetActive(false);
-                        // Choose the Andy model for the Trackable that got hit.
-                        //GameObject prefab;
+   
                         item = PlayerPrefs.GetInt("item", 1);
                         if (hit.Trackable is FeaturePoint)
                         {
+                            hint.gameObject.SetActive(false);
                             switch (item)
                             {
                                 case 1:
@@ -234,6 +191,7 @@
                         }
                         else
                         {
+                            hint.gameObject.SetActive(false);
                             switch (item)
                             {
                                 case 1:
@@ -286,42 +244,21 @@
                                     prefab = null;
                                     break;
                             }
+                           
 
-                            
 
                         }
-                    
-                        /*// Instantiate Andy model at the hit pose.
-                        var andyObject = Instantiate(prefab, hit.Pose.position, hit.Pose.rotation);
-                        DetectedPlanePrefab.SetActive(false);
-                        // Compensate for the hitPose rotation facing away from the raycast (i.e. camera).
-                        andyObject.transform.Rotate(0, k_ModelRotation, 0, Space.Self);
-
-                        // Create an anchor to allow ARCore to track the hitpoint as understanding of the physical
-                        // world evolves.
-                        var anchor = hit.Trackable.CreateAnchor(hit.Pose);
-                        BL.gameObject.SetActive(true);
-                        BR.gameObject.SetActive(true);
-                        // Make Andy model a child of the anchor.
-                        andyObject.transform.parent = anchor.transform;*/
                         
                     }
                 }
             }
         }
-
-        /// <summary>
-        /// Check and update the application lifecycle.
-        /// </summary>
         private void _UpdateApplicationLifecycle()
-        {
-            // Exit the app when the 'back' button is pressed.
+        {          
             if (Input.GetKey(KeyCode.Escape))
             {
                 SceneManager.LoadScene("SelectionMenu");
             }
-
-            // Only allow the screen to sleep when not tracking.
             if (Session.Status != SessionStatus.Tracking)
             {
                 const int lostTrackingSleepTimeout = 15;
@@ -337,7 +274,6 @@
                 return;
             }
 
-            // Quit if ARCore was unable to connect and give Unity some time for the toast to appear.
             if (Session.Status == SessionStatus.ErrorPermissionNotGranted)
             {
                 _ShowAndroidToastMessage("Camera permission is needed to run this application.");
@@ -351,19 +287,10 @@
                 Invoke("_DoQuit", 0.5f);
             }
         }
-
-        /// <summary>
-        /// Actually quit the application.
-        /// </summary>
         private void _DoQuit()
         {
             Application.Quit();
         }
-
-        /// <summary>
-        /// Show an Android toast message.
-        /// </summary>
-        /// <param name="message">Message string to show in the toast.</param>
         private void _ShowAndroidToastMessage(string message)
         {
             AndroidJavaClass unityPlayer = new AndroidJavaClass("com.unity3d.player.UnityPlayer");
